2512583545f13dece0535ba077864b3df0b8781a
GAlencastro <38190063+GAlencastro@users.noreply.github.com>
2019-06-22 14:46:07 -0300

    Add files via upload

--- src/gui/FRJogo.java
+++ src/gui/FRJogo.java
@@ -3,11 +3,13 @@ package gui;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 import javax.swing.*;
 
-import menuIni.MenuInicial;
-import utils.Dados; 
+import MenuIni.MenuInicial; 
 
 public class FRJogo extends JFrame{
 	  final int LARG_DEFAULT=1200;
@@ -17,6 +19,17 @@ public class FRJogo extends JFrame{
 		    setLayout(new FlowLayout());
 		    Toolkit tk=Toolkit.getDefaultToolkit();
 			Dimension screenSize=tk.getScreenSize();
+			
+			JMenuBar menu = new JMenuBar();
+			JMenu salvamento = new JMenu("Salvar jogo");
+	        menu.add(salvamento);
+	        salvamento.addActionListener(new ActionListener() {
+	        	public void actionPerformed(ActionEvent e) {
+	        		
+	        	}
+	        });
+	        setJMenuBar(menu);
+			
 			int sl=screenSize.width;
 			int sa=screenSize.height;
 			int x=sl/2-LARG_DEFAULT/2;
@@ -29,23 +42,31 @@ public class FRJogo extends JFrame{
 			tab.addJogadores(num); // retirar daqui depois 
 			
 			tab.setPreferredSize(new Dimension(700,700));	
-			bot.setPreferredSize(new Dimension(400,700));	
+			bot.setPreferredSize(new Dimension(300,300));	
 			bot.setBorder(BorderFactory.createTitledBorder("Controles"));
 			
 			getContentPane().add(tab);
 			getContentPane().add(bot);
 			
-		
+			pack(); 
 			setTitle("Banco Imobiliario");
 			setVisible(true); 
 	   }  
 	   
 	   public static void main(String args[]) {
-		    Factory fac = Factory.startFactory(); 
-		    MenuInicial menuIni = new MenuInicial();
-			
+		     Factory fac = Factory.startFactory();
+		     MenuInicial menuIni = new MenuInicial();
+		
 		
 		}
 	   
+	  private void saveGame(File f) throws IOException {
+		  FileOutputStream fileStream = new FileOutputStream(f);   
+	      ObjectOutputStream objectStream = new ObjectOutputStream(fileStream);
+	      
+	      objectStream.writeObject(MenuInicial.Monopoly);
+	      objectStream.close();
+	      fileStream.close();
 	  
+	  }
 }
